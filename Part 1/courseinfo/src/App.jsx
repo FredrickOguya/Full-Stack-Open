c57822a7882329c4ce3c9bcd966998e21897565b
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-10-26 12:39:01 +0300

    1.4 redone

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -3,27 +3,22 @@ import React from 'react';
     return <h1>{props.course}</h1>;
   };
 
-  const Content = (props) => {
-
-    const Part = (props) => {
-      return (
-        <p>
-          {props.part} {props.exercises}
-        </p>
-      );
-    };
+  const Content = ({parts}) => {
+    const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+  
     return (
       <div>
-        <Part part={props.part1.name} exercises={props.exercises1} />
-        <Part part={props.part2.name} exercises={props.exercises2} />
-        <Part part={props.part3.name} exercises={props.exercises3} />
+        {parts.map((part,index)=> (
+          <Part key={index} name={part.name} exercises={part.exercises} />
+        ))}
       </div>
     );
   };
-  const Total = (props) => {
+  const Total = ({parts}) => {
+    const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
     return (
       <p>
-        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        Number of exercises {totalExercises}
       </p>
     );
   };      
@@ -43,15 +38,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={parts[0]} exercises1={parts[0].exercises}
-        part2={parts[1]} exercises2={parts[1].exercises}
-        part3={parts[2]} exercises3={parts[2].exercises}
-      />
-      <Total 
-        exercises1={parts[0].exercises} 
-        exercises2={parts[1].exercises} 
-        exercises3={parts[2].exercises} 
+      <Content parts={parts} />
+      <Total parts={parts} 
       />
     </div>
   );
