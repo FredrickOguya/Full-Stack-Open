24d910bfc49c1ec1857ba2ffb67f8ac3a71eb30d
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-10-26 12:00:57 +0300

    1.3 Course Information step 3

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -14,9 +14,9 @@ import React from 'react';
     };
     return (
       <div>
-        <Part part={props.part1} exercises={props.exercises1} />
-        <Part part={props.part2} exercises={props.exercises2} />
-        <Part part={props.part3} exercises={props.exercises3} />
+        <Part part={props.part1.name} exercises={props.exercises1} />
+        <Part part={props.part2.name} exercises={props.exercises2} />
+        <Part part={props.part3.name} exercises={props.exercises3} />
       </div>
     );
   };
@@ -29,25 +29,32 @@ import React from 'react';
   };      
 const App = () => { 
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   const exercises3 = 14;
 
   return (
     <div>
       <Header course={course} />
       <Content 
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
+        part1={part1} exercises1={part1.exercises}
+        part2={part2} exercises2={part2.exercises}
+        part3={part3} exercises3={part3.exercises}
       />
       <Total 
-        exercises1={exercises1} 
-        exercises2={exercises2} 
-        exercises3={exercises3} 
+        exercises1={part1.exercises} 
+        exercises2={part2.exercises} 
+        exercises3={part3.exercises} 
       />
     </div>
   );
