df7bda401a4771433d1fbf99d0842181e46116b7
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-11-29 23:21:46 +0300

    2.8 The Phonebook Step 3

--- Part 2/the phonebook/src/App.jsx
+++ Part 2/the phonebook/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState()
 
 
   const addNewNumber = (event) => {
     event.preventDefault();
     const numberObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     const alreadyExists = persons.some(person => person.name ===newName)
     if(alreadyExists) {
@@ -18,12 +20,9 @@ const App = () => {
     }else{
       setPersons([...persons,numberObject]);
       setNewName('');
+      setNewNumber('')
     }
     
-  }
-
-  const handleChange = (event) => {
-    setNewName(event.target.value);
   }
   
 
@@ -35,7 +34,12 @@ const App = () => {
           name: <input
                  type='text'
                  value={newName}
-                 onChange={handleChange} 
+                 onChange={(event) => setNewName(event.target.value)} 
+                />
+          number: <input
+                 type='phone'
+                 value={newNumber}
+                 onChange={(event) => setNewNumber(event.target.value)}
                 />
         </div>
         <div>
@@ -45,7 +49,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         { persons.map(persons=> 
-          <p key={persons.name}>{persons.name}</p>
+          <p key={persons.name}>{persons.name} {persons.number}</p>
         )}
       </div>
       
