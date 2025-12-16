7f942a1b45a602963300bb81c1948095fbbe390e
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-12-16 18:16:16 +0300

    3.8*: Phonebook backend step 8

--- Part 2/the phonebook/backend/index.js
+++ Part 2/the phonebook/backend/index.js
@@ -27,6 +27,11 @@ const app = express();
 
 app.use(morgan('tiny'))
 app.use(express.json());
+app.use(morgan(':method : url :status :res[content-length] - :response-time ms :body'));
+
+morgan.token('body', (req)=> {
+  return req.method === 'POST' ? JSON.stringify(req.body): '';
+})
 
 app.get('/api/persons',(request,response) => {
   response.json(persons)
