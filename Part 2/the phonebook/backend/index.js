186c57262f97e791786b91ddec0ef6b5099fc78d
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-12-19 20:51:43 +0300

    3.11 Full Stack Phonebook

--- Part 2/the phonebook/backend/index.js
+++ Part 2/the phonebook/backend/index.js
@@ -31,6 +31,7 @@ app.use(morgan('tiny'))
 app.use(express.json());
 app.use(morgan(':method : url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());
+app.use(express.static('dist'));
 
 morgan.token('body', (req)=> {
   return req.method === 'POST' ? JSON.stringify(req.body): '';
@@ -82,7 +83,7 @@ app.post('/api/persons',(request,response)=> {
   }
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, ()=> {
   console.log(`app listening on port ${PORT}`)
