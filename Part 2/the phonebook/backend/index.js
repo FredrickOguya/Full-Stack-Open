a9de418d83249387a8693fabb1e4b780d101e28d
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-01-11 10:53:43 +0300

    3.13: Phonebook database, step 1

--- Part 2/the phonebook/backend/index.js
+++ Part 2/the phonebook/backend/index.js
@@ -1,29 +1,31 @@
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
+let persons = [];
 
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://onyangofredrickoguya:${password}@cluster0.hxovx14.mongodb.net/phonebook?appName=Cluster0`
+
+mongoose.set('strictQuery',false);
+mongoose.connect(url, {family: 4})
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number
+})
+personSchema.set('toJSON',{
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema);
 
 const app = express();
 
@@ -38,19 +40,21 @@ morgan.token('body', (req)=> {
 })
 
 app.get('/api/persons',(request,response) => {
-  response.json(persons)
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
 app.get('/api/persons/:id',(request,response)=> {
-  const id = request.params.id;
-  
-  const person = persons.find(n=> n.id === id);
-  if(person){
-    response.send(person)
-  } else {
-    response.status(404).end();
-  }
-  
+  Person.findById(request.params.id)
+  .then(person => {
+    if(person){
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => response.status(404).send({error: "error in id"}))
 })
 
 app.get('/info',(request,response)=> {
@@ -60,27 +64,34 @@ app.get('/info',(request,response)=> {
 
 app.delete('/api/persons/:id',(request,response)=>{
   const id = request.params.id;
-  const deletePerson = persons.find(n=> n.id === id);
 
-  persons = persons.filter(person=> person.id !== deletePerson.id);
-  response.send(persons);
+  if (id === 'undefined'){
+  return response.status(400).send({error: 'id is not defined'})
+  }
+  Person.findByIdAndDelete(id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(400).send({error: 'wrong id'})
+  })
 })
 
 app.post('/api/persons',(request,response)=> {
-  const id = Math.floor(Math.random() * 100)
-  const person = request.body
-  if(!person.name || !person.number ){
-    return response.status(400).json({
-      error: "name and number must have values"
-    })
-  }else if(persons.find(p => p.name === person.name)){
-    return response.status(400).json({
-      error: "name must be unique"
-    })
-  }else{
-    person.id = String(id);
-    response.json(person);
+  const body = request.body
+  if(!body.name || !body.number){
+    return response.status(400).json({error: 'name or number missing'})
   }
+  
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 const PORT = process.env.PORT || 3001;
