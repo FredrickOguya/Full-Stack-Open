4e0c0b93dbe372a6cd25812bfdbcf3ef70a6a353
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-01-30 12:42:24 +0300

    4.4: Helper Functions and Unit Tests, step 2

--- Part 4/utils/list_helper.js
+++ Part 4/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = () => {
   return 1
 }
 
+const totalLikes = (array) => {
+  const posts = array;
+
+  const total = posts.reduce((accumulator, currentPost) => {
+    return accumulator + currentPost.likes
+  }, 0)
+
+  return total
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
