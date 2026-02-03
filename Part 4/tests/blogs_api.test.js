2e81b6243b372bb05cadd1fe4e8698bf4e062143
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-02-03 20:42:52 +0300

    4.10: Blog List Tests, step 3

--- Part 4/tests/blogs_api.test.js
+++ Part 4/tests/blogs_api.test.js
@@ -39,12 +39,35 @@ test('blogs are returned as json and correct amount',async () => {
     assert.strictEqual(reponse.body.length,initialBLogs.length)
 })
 
-test.only('blog posts have id property instead of _id', async () => {
+test('blog posts have id property instead of _id', async () => {
   const response = await api.get('/api/blogs')
   response.body.forEach(blog => {
     assert.ok(blog.id)
     assert.strictEqual(blog._id, undefined)
   })
+})
+
+test.only('a valid blog can be added via POST', async () => {
+  const newBlog = {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await Blog.find({})
+    assert.strictEqual(blogsAtEnd.length, initialBLogs.length + 1)
+
+    const titles = blogsAtEnd.map(n => n.title)
+    assert(titles.includes('Canonical string reduction'))
 })
 
 after(async () => {
