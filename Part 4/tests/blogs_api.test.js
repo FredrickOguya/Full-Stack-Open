6e17fbfd0af02e4709024d52b34faa0d02b82009
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-02-03 20:30:36 +0300

    4.9: Blog List Tests, step 2

--- Part 4/tests/blogs_api.test.js
+++ Part 4/tests/blogs_api.test.js
@@ -39,6 +39,14 @@ test('blogs are returned as json and correct amount',async () => {
     assert.strictEqual(reponse.body.length,initialBLogs.length)
 })
 
+test.only('blog posts have id property instead of _id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach(blog => {
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  })
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
