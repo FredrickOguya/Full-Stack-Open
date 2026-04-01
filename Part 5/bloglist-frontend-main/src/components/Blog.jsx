c1bc992a5c0320364d7a0497dfa6d3110a90acd0
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-04-01 10:50:36 +0300

    5.7 Blog List Frontend, step 7

--- Part 5/bloglist-frontend-main/src/components/Blog.jsx
+++ Part 5/bloglist-frontend-main/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const label = visible ? 'hide' : 'view'
+
+  const showWhenVisible = { display: visible ? '' : 'none'}
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{label}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>
+          likes {blog.likes}
+          <button>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+      
+    </div>
+
+  )
+}
 
 export default Blog
