7d2967369d51b18ad5dfc3a3a8ddb3b0b46a999c
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-07-01 10:06:19 +0300

    6.7 anecdotes, step6

--- Part 6/zustand-anecdotes/src/App.jsx
+++ Part 6/zustand-anecdotes/src/App.jsx
@@ -1,11 +1,18 @@
 
+import { useEffect } from 'react'
 import AnecdoteForm from './AnecdoteForm'
 import AnecdoteList from './AnecdoteList'
 import Filter from './Filter'
+import anecdoteService from './services/anecdotes'
+import { useAnecdoteActions } from './store'
 
 const App = () => {
 
+  const { initialize } = useAnecdoteActions()
 
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => initialize(anecdotes))
+  }, [initialize])
   return (
     <div>
       <Filter/>
