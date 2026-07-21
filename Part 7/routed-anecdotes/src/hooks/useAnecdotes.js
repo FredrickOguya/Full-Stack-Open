9348f7ad07ee6ada85b184147a8262ffdd99e6ed
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-07-21 15:34:39 +0300

    7.4: useAnecdotes, step1

---
+++ Part 7/routed-anecdotes/src/hooks/useAnecdotes.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react"
+import anecdoteService from '../services/anecdotes'
+
+export const useAnecdotes = () => {
+    const [anecdotes, setAnecdotes] = useState([])
+
+    useEffect(() => {
+        anecdoteService.getAll().then(data => setAnecdotes(data))
+    }, [])
+
+    return anecdotes
+}
